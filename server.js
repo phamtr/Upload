2571bf38d50ea5b3a1fea0fdfef07cba037ca566
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-09-16 18:51:40 +0700

    add

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const upload = require('./upload');
+const download = require('./download');
+const deletefile = require('./deletefile');
 const cors = require('cors');
 
 const server = express();
@@ -11,7 +13,12 @@ var corsOptions = {
 
 server.use(cors(corsOptions));
 
+var publicDir = require('path').join(__dirname,'/data');
+server.use('/resources', express.static(publicDir));
+
 server.post('/upload', upload);
+server.get('/download', download);
+server.delete('/deletefile', deletefile);
 
 server.listen(8000, () => {
     console.log('Server started!');
--- upload.js
+++ upload.js
@@ -5,18 +5,22 @@ var path = require('path');
 module.exports = function upload(req, res) {
     var form = new formidable.IncomingForm();
 
+    form.maxFieldsSize = 10*1024*1024;
+
+
     form.parse(req);
 
     form.on('fileBegin', function (name, file){
         
-        file.path = __dirname + '/data/' + 'Photo-' + Date.now()+ file.name;
+        file.path = __dirname + '/data/' + file.name;
     });
    
     form.on('file', function (name, file){
-        console.log('Uploaded ' + 'Photo-' + Date.now()+ file.name);
+        console.log('Uploaded ' + file.name);
     });
-
+  
     return res.json(200, {
 	result: 'Upload Success'
     });
+   
 };
